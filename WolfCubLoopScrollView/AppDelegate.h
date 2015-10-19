f65fabd5611499ed5b816d0841f1451bbbdfad63
LeiLeiYe <leileiye@leileiyedemacbook-pro.local>
2015-10-19 13:53:41 +0800

    Initial Commit

---
+++ WolfCubLoopScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WolfCubLoopScrollView
+//
+//  Created by LeiLeiYe on 15/10/19.
+//  Copyright © 2015年 WolfCub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WolfCubLoopScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WolfCubLoopScrollView
+//
+//  Created by LeiLeiYe on 15/10/19.
+//  Copyright © 2015年 WolfCub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
